c3689cfe50d7ba9742bddc9f48a87e206e9d74df
dmoore <darren.m95@icloud.com>
2021-05-02 23:53:08 +0100

    Add comments to hello world endpoint

--- backend/src/routes/helloWorld.ts
+++ backend/src/routes/helloWorld.ts
@@ -3,6 +3,9 @@ import helloWorldController from '../controllers/helloWorld';
 
 const router: Router = express.Router();
 
+// @route   GET /
+// @desc    Get 'Hello World'
+// @access  Public
 router.get('/', helloWorldController.getHello);
 
 export default router;
